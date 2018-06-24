63cd3e13420ab90ac3084189544013e5eadfc06c
Isaiah Gaspar <igaspar@asu.edu>
2018-06-24 16:16:11 -0700

    BPM Parameters
    Syncing data on sample number, beat count, time, and electrical activity to later derive BPM between different regions of activity.

--- testML_Code/testData1.m
+++ testML_Code/testData1.m
@@ -1,27 +1,39 @@
 %Read data into workspace
 testData = xlsread('0testData.xlsx','Sheet1','A1:C721950');
 
-%Select all rows and first column for x1
-x1_minutes = testData(:,1);  
+%Select all rows and first column for x
+x = testData(:,1);  
 
-%Select all rows and column 3 for y1
-y1_eeg = testData(:,3);    
+%Select all rows and column 3 for y
+y = testData(:,3);    
 
 %Plot x-y with ecg script for sliding window, also showing red line with
 %markers 'o' at every sample
 
-plotECG(x1_minutes,y1_eeg, 'r-o')
-hold on
-title('Plot of ECG')
-xlabel("Time [ms]")
-ylabel("ECG Biopotential [uV?]")
+% title('Plot of ECG')
+% plot(x,y, 'r-o')
+% hold on
+% xlabel("Time [ms]")
+% ylabel("ECG Biopotential [uV?]")
 
 beat_count=0;
-for k = 2 :length(y1_eeg)-1
-    if((y1_eeg(k) > y1_eeg(k-1)) && (y1_eeg(k) > y1_eeg(k+1)) && (y1_eeg(k) > 1.8) && (y1_eeg(k) < 3.0))
-        k
-        disp('Prominant Peak')
+for k = 2 :length(y)-1
+    if((y(k) > y(k-1)) && (y(k) > y(k+1)) && (y(k) > 1.8) && (y(k) < 2.3))
+        
         beat_count = beat_count + 1;
-        beat_count
-    end
+       
+        time = testData(k,1);
+        actv = testData(k,3);
+        
+        results_eeg(beat_count,1) = k;
+        results_eeg(beat_count,2) = beat_count;
+        results_eeg(beat_count,3) = time;
+        results_eeg(beat_count,4) = actv;
+        
+        
+    end 
+    
+       
+    
 end
+
